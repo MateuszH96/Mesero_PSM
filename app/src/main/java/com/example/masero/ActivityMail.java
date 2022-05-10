5e92c1c60fda0a4fd3dd3349249ba2aa16e1d206
Mateusz Hamera <mateuszhamera2@gmail.com>
2022-05-10 18:44:53 +0200

    dodawanie/zmiana maila

--- app/src/main/java/com/example/masero/ActivityMail.java
+++ app/src/main/java/com/example/masero/ActivityMail.java
@@ -2,13 +2,50 @@ package com.example.masero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityMail extends AppCompatActivity {
 
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail);
+        editText=findViewById(R.id.mailToSet);
+        checkIsEmail();
+    }
+
+    public void clickSaveMail(View view) {
+        saveData(editText.getText().toString());
+    }
+
+    private void saveData(String input) {
+        SharedPreferences sharedPreferences = getSharedPreferences("myPref",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("EMAIL_KEY",input);
+        editor.apply();
+        Toast.makeText(this,"ustawiono mail: \n"+input,Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(ActivityMail.this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    private String getData(){
+        String text;
+        SharedPreferences sharedPreferences = getSharedPreferences("myPref",MODE_PRIVATE);
+        text = sharedPreferences.getString("EMAIL_KEY","");
+        return text;
+    }
+    private void checkIsEmail() {
+        String mail=getData();
+        if (mail!=""){
+            editText.setText(mail);
+        }
     }
 }
--- app/src/main/java/com/example/masero/MainActivity.java
+++ app/src/main/java/com/example/masero/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.masero;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,36 +20,49 @@ import Database.GetListDish;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView1;
-    TextView textView2;
-    TextView textView3;
     Connect connect;
     Thread thread;
+    Button newOrderButton;
+    Button prevOrdersButton;
+    Button yourOrdersButton;
+    public static final String SHARED_PREF = "sharedPreferences";
+    public static final String EMAIL_KEY = "email_key_check";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkIsEmail();
+        newOrderButton = findViewById(R.id.newOrder);
+        prevOrdersButton = findViewById(R.id.previousOrders);
+        yourOrdersButton = findViewById(R.id.yourOrder);
+        setButtonsClickable(true);
         thread = new Thread(() -> {
             connect = new Connect();
             if (connect.getConnection()==null){
-                textView1.setText("jest problem");
+                Toast.makeText(this,"Nie można połączyć z bazą",Toast.LENGTH_LONG).show();
+                setButtonsClickable(false);
             }
         });
         thread.start();
     }
-    public void ButtonClick(View view) throws InterruptedException {
-        /*GetListDish getListDish=new GetListDish(connect);
-        //Toast.makeText(this,"Pobieranie danych...",Toast.LENGTH_SHORT).show();
-        while(!getListDish.getStatus()) {
-            Thread.sleep(100);
-        }
-        //System.out.println(getListDish.getListOfDish().get(0).getName());
 
-        setTextView(textView1,getListDish.getListOfDish().get(0));
-        setTextView(textView2,getListDish.getListOfDish().get(1));
-        setTextView(textView3,getListDish.getListOfDish().get(2));*/
+    private void setButtonsClickable(boolean toSet) {
+        newOrderButton.setClickable(toSet);
+        prevOrdersButton.setClickable(toSet);
+        yourOrdersButton.setClickable(toSet);
     }
-    void setTextView(TextView textView,Dish dish){
-        textView.setText(MessageFormat.format("{0} {1}g {2}zł", dish.getName(), dish.getWeight().toString(), dish.getPrice().toString()));
+    private String getData(){
+        String text;
+        SharedPreferences sharedPreferences = getSharedPreferences("myPref",MODE_PRIVATE);
+        text = sharedPreferences.getString("EMAIL_KEY","");
+        return text;
+    }
+    private void checkIsEmail() {
+        if (getData()==""){
+            Intent intent = new Intent(MainActivity.this,ActivityMail.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     public void previousOrder(View view) {
@@ -60,10 +76,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setMail(View view) {
-        SendMail.sendMailMessage(this,
+        /*SendMail.sendMailMessage(this,
                                 "mateuszhamera3@gmail.com",
                                 "hello",
-                                "hello there");
+                                "hello there");*/
         Intent intent = new Intent(MainActivity.this,ActivityMail.class);
         startActivity(intent);
     }
